91f49acaa4206dcf01bd0a6a18f6c15a6c758a5b
Takogero <149505416+Takogero@users.noreply.github.com>
2024-04-20 13:33:20 +0300

    Initial Commit

---
+++ SocialNetwork/SocialNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialNetworkApp.swift
+//  SocialNetwork
+//
+//  Created by MacBookAir on 20.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SocialNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
