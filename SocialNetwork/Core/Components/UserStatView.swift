06fbd8605aae777b1bd85e486a8cbe74bcb20be0
Takogero <149505416+Takogero@users.noreply.github.com>
2024-04-22 17:41:08 +0300

    доделал профиль

---
+++ SocialNetwork/Core/Components/UserStatView.swift
@@ -0,0 +1,32 @@
+//
+//  UserStatView.swift
+//  SocialNetwork
+//
+//  Created by MacBookAir on 22.04.2024.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    let value: Int
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            
+            Text(title)
+                .font(.footnote)
+        }
+        .frame(width: 76)
+    }
+}
+
+
+struct UserStatView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserStatView(value: 12, title: "Posts")
+    }
+}
