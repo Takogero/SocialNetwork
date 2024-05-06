688b4e29ae90c757c3e49e70629b78bdc678cfda
Takogero <149505416+Takogero@users.noreply.github.com>
2024-05-06 20:58:59 +0300

    edit profile view

--- SocialNetwork/Core/Profile/View/ProfileHeaderView.swift
+++ SocialNetwork/Core/Profile/View/ProfileHeaderView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ProfileHeaderView: View {
     let user: User
+    @State private var showEditProfile = false
     
     var body: some View {
             //pic and stats
@@ -52,7 +53,7 @@ struct ProfileHeaderView: View {
                 //action button
                 Button {
                     if user.isCurrentUser {
-                        print("Show edit profile")
+                        showEditProfile.toggle()
                     } else {
                         print("Follow user..")
                     }
@@ -71,7 +72,9 @@ struct ProfileHeaderView: View {
                 
                 
                 Divider()
-                
+            }
+            .fullScreenCover(isPresented: $showEditProfile) {
+                Text("Edit profile view")
             }
         }
     }
---
+++ SocialNetwork/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  EditProfileViewModel.swift
+//  SocialNetwork
+//
+//  Created by MacBookAir on 06.05.2024.
+//
+
+import SwiftUI
+import Firebase
+import PhotosUI
+
+
+@MainActor
+class EditProfileViewModel: ObservableObject {
+    @Published var selectedImage: PhotosPickerItem? {
+        didSet { Task { await loadImage(fromItem: selectedImage) } }
+    }
+    @Published var profileImage: Image?
+    @Published var fullname = ""
+    @Published var bio = ""
+    
+    func loadImage(fromItem item: PhotosPickerItem?) async {
+        guard let item = item else { return }
+        
+        guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+        guard let uiImage = UIImage(data: data) else { return }
+        self.profileImage = Image(uiImage: uiImage)
+    }
+}
+
